4db2a44995f9c3467ae808ce01c39ea7311a4784
aziz-kacho <76108391+aziz-kacho@users.noreply.github.com>
2024-11-03 00:45:36 +0500

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ChatApp"
+include(":app")
+include(":core")
+include(":feature_auth")
+include(":feature_chat")
+include(":feature_profile")
